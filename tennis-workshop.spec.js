3d988808d6a3845c29abbf953eeb19f6bf92d38e
charintip jantawong <58160378@go.buu.ac.th>
2017-09-15 10:12:18 +0700

    test past

--- tennis-workshop.spec.js
+++ tennis-workshop.spec.js
@@ -1,3 +1,12 @@
+function TennisGame() {
+  this.reset = () => {
+
+  }
+  this.echo = () => {
+    return 'Love Love'
+
+  }
+}
 test('Echo "Love Love" when game starts', () => {
   //Arrange
   let app = new TennisGame()
